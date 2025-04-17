e6c8a36003115ba12b5a6b492fd30dcd244e3c76
ngocanh <tnanhhdeptrai@gmail.com>
2025-04-17 17:48:59 +0700

    22/4

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +23,24 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+    // roleId
+    // User many to one -> role
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
     public long getId() {
         return id;
@@ -68,6 +93,7 @@ public class User {
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+
 }
